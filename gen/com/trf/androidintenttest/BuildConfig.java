d55a2f284e015209d5cfff8b3bac20d4eec193da
XXFatai <trf669045207@126.com>
2013-03-01 20:19:15 +0800

    android intent学习

---
+++ gen/com/trf/androidintenttest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.trf.androidintenttest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/trf/androidintenttest/Activity2.java
@@ -0,0 +1,14 @@
+package com.trf.androidintenttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class Activity2 extends Activity{
+
+	
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_activity2);
+	}
+}
